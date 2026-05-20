826c87b537038fd9d0767e6d5f7abdf966cbf47d
z0nT <kosty-01@inbox.ru>
2026-05-20 19:13:03 +1000

    Добавил в еще в меню еще одно действие.

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ while True:
     print("1 - покормить")
     print("2 - уложить спать")
     print("3 - мяукнуть")
-    print("4 - климат")
-    print("5 - выйти из программы")
+    print("4 - побегать")
+    print("5 - климат")
+    print("6 - выйти из программы")
 
     chois = input("Выбирите действие что вы хотите сделать с котом: ")
     if chois == "1":
@@ -23,6 +24,9 @@ while True:
         pass
 
     elif chois == "5":
+        pass
+
+    elif chois == "6":
         break
 
 
